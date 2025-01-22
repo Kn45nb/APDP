e691cf24dd905ccaead0a7cf6b182ead090ae4c2
Kiên <110784389+Kn45nb@users.noreply.github.com>
2025-01-22 20:43:47 +0700

    Update Program.cs

--- app/Program.cs
+++ app/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LibraryCatalog catalog = new LibraryCatalog();
+
+            Book book1 = new Book("Getting rich is not difficult!(Làm giàu không khó!)", "Nguyen Hieu Kien", 2024 , true, "Fiction");
+            DVD dvd1 = new DVD("Vlog: A normal day of a dev (Vlog: Một ngày bình thường của dev)", "Kn45nb (Kiên)", 2023, true, 999);
+
+            catalog.addItem(book1);
+            catalog.addItem(dvd1);
+
+            Console.WriteLine("Library Catalog:");
+            catalog.findItem("");
+
+            Console.WriteLine("\nChecking out 'The Great Gatsby':");
+            book1.checkout();
+
+            Console.WriteLine("\nLibrary Catalog:");
+            catalog.findItem("");
+
+            Console.WriteLine("\nReturning 'The Great Gatsby':");
+            book1.returnItem();
+
+            Console.WriteLine("\nLibrary Catalog:");
+            catalog.findItem("");
+        }
+    }
+}
