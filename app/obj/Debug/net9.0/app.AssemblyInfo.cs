b42fd3be64399a2d451100c8d277898e8aebf148
Kiên <110784389+Kn45nb@users.noreply.github.com>
2025-01-22 20:36:13 +0700

    LibraryCatalog

--- app/obj/Debug/net9.0/app.AssemblyInfo.cs
+++ app/obj/Debug/net9.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60cfac27fb71477c92f71a099dc96cd30abb8f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94aba8da3269598df23dd714ba0d053e15e5b8bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
