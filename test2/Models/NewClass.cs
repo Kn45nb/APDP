9052eaad3a91de939456a749473ee627e1320d1d
Kiên <knight.45.acgg@gmail.com>
2025-03-04 13:17:25 +0700

    Update NewClass.cs

--- test2/Models/NewClass.cs
+++ test2/Models/NewClass.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,16 @@ namespace test2.Models
 {
     public class NewClass
     {
+        [Key]
         public int Id {get;set;}
+        [Required]
+        [StringLength(50)]
         public string Username {get;set;}
+        [Required]
+        [StringLength(50)]
         public string Password {get;set;}
+        [Required]
+        [StringLength(50)]
         public string Email {get;set;}
     }
 }
